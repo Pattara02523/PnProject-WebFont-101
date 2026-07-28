5695079a3beabf5f9ebe2f851bdc17f99e51ae3e
Pattara02523 <Pattarapon.thongsri@gmail.com>
2026-07-29 04:20:07 +0700

    feat: setup shadcn ui and auth login/register pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,26 @@
 import '@/styles/globals.css';
 
+import { cn } from '@/lib/utils';
+import { notoSans } from '@/styles/font';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | InvestmentPro',
+    default: 'InvestmentPro',
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`antialiased`}>
+    <html
+      lang="en"
+      className={cn('antialiased', 'font-sans', notoSans.variable)}
+    >
       <body>{children}</body>
     </html>
   );
